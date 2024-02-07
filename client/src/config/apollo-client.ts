5df00eddf096e314a7d1db46e4659354b8f92056
npoulain <36767700+nicolaspln@users.noreply.github.com>
2024-02-07 18:19:04 +0100

    feat(apollo-client): logout link:

--- client/src/config/apollo-client.ts
+++ client/src/config/apollo-client.ts
@@ -1,9 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, concat } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import env from "./env";
+import excludedRoutes from "./excluded-routes";
+import router from "../components/Routes";
+
+const logoutLink = onError((error) => {
+  if (
+    Array.isArray(error.graphQLErrors) &&
+    error.graphQLErrors.length > 0 &&
+    error.graphQLErrors.some(
+      (e) => e?.extensions?.originalError?.statusCode === 401
+    )
+  ) {
+    if (!excludedRoutes.includes(window.location.pathname)) {
+      router.navigate("/login");
+      client.resetStore();
+    }
+  }
+});
+
+const httpLink = new HttpLink({ uri: `${env.urls.api}/graphql` });
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: `${env.urls.api}/graphql`,
+  link: concat(logoutLink, httpLink),
 });
 
 export default client;
