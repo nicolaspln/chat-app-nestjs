7fe224d36e0f30273440e64bd6a5d8e1ee1b10a7
npoulain <36767700+nicolaspln@users.noreply.github.com>
2024-02-12 15:46:38 +0100

    fix: set user email to lower case

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -2,10 +2,12 @@ import { Field, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { AbstractEntity } from 'src/common/database/abstract.entity';
 
+const toLowerCase = (v: string) => v.toLowerCase(); // to be tested once we implement logout
+
 @Schema({ versionKey: false })
 @ObjectType()
 export class User extends AbstractEntity {
-  @Prop()
+  @Prop({ set: toLowerCase })
   @Field()
   email: string;
 
